ac4be6601e4df14eb52be1bbfc61e2e2a50ef0bd
Black Sheep <98521682+BlackSheep47@users.noreply.github.com>
2024-10-25 09:28:57 +0530

    Add files via upload

--- Mac-add-changer/mac-changer.py
+++ Mac-add-changer/mac-changer.py
@@ -1,7 +1,20 @@
 # Mac Address Changer 
 
 import subprocess # for more info https://docs.python.org/3/library/subprocess.html
-subprocess.call("ifconfig eth0 down", shell=True) #system command using subprocess (change ip for if for linux)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+
+subprocess.call("ifconfig", shell=True) # show present network card present 
+
+print("Choose your network card !!")
+
+networkcard = input("Enter your Network card name: ") # select you network card for example wlan0 eth0 etc
+
+subprocess.call(f"ifconfig {networkcard} down", shell=True) # system command using subprocess (change ip for if for linux)
+
+Macaddress = input("Enter your custom Mac Address: ")
+
+subprocess.call(f"ifconfig {networkcard} hw ether {Macaddress}", shell=True)
+
+subprocess.call(f"ifconfig {networkcard} up", shell=True)
+
+
 
